2ca4bfeb1fb476cbde21cac415d1c3829ac03a0f
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-05-10 16:50:52 +0100

    task 0

--- utils/redis.js
+++ utils/redis.js
@@ -1,1 +1,32 @@
 #!/usr/bin/node
+
+import { createClient } from 'redis-promisify';
+
+class RedisClient {
+	constructor() {
+		this.client = createClient()
+			.on('error', (err) => {
+				console.log('Error connecting to Redis: ' + err);
+			});
+	}
+
+	isAlive() {
+		return this.client.connected ? true : false;
+	}
+
+	async get(key) {
+		return await this.client.getAsync(key);
+	}
+
+	async set(key, value, duration) {
+		await this.client.setAsync(key, value);
+		await this.client.expireAsync(key, duration);
+	}
+
+	async del(key) {
+		await this.client.delAsync(key);
+	}
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
