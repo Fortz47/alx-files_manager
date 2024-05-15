920a5f5b2c278e819f4ea5e9ea6f72ebea213108
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-05-15 12:24:39 +0100

    fix requirement fails

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -8,14 +8,14 @@ class AppConroller {
     const redisStatus = redisClient.isAlive();
     const dbStatus = dbClient.isAlive();
     const status = { redis: redisStatus, db: dbStatus };
-    res.json(status);
+    res.status(200).json(status).end();
   }
 
   static async getStat(req, res) {
     const nbUsers = await dbClient.nbUsers();
     const nbFiles = await dbClient.nbFiles();
     const stat = { users: nbUsers, files: nbFiles };
-    res.json(stat);
+    res.status(200).json(stat).end();
   }
 }
 
