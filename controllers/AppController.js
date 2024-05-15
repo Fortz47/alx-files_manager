7be2b51d112c5cdac0064c0250f5bf13b25d325d
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-05-15 12:41:35 +0100

    Update AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,19 +3,19 @@
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-class AppConroller {
-  static getStatus(req, res) {
+class AppController {
+  static async getStatus(request, response) {
     const redisStatus = redisClient.isAlive();
     const dbStatus = dbClient.isAlive();
-    const status = { redis: redisStatus, db: dbStatus };
-    res.status(200).json(status).end();
+    response.set('Content-Type', 'application/json');
+    response.status(200).json({ redis: redisStatus, db: dbStatus }).end();
   }
 
-  static async getStat(req, res) {
-    const nbUsers = await dbClient.nbUsers();
-    const nbFiles = await dbClient.nbFiles();
-    const stat = { users: nbUsers, files: nbFiles };
-    res.status(200).json(stat).end();
+  static async getStats(request, response) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    response.set('Content-Type', 'application/json');
+    response.status(200).json({ users, files }).end();
   }
 }
 
