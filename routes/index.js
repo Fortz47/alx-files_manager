7aaa7c3987e0fdad5a3aac4633be56246533c8d1
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-05-15 13:21:30 +0100

    added express.Router for loading routes

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,19 @@
+import express from 'express';
 import AppConroller from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
 
-const loadRoutes = (app) => {
-  app.get('/status', AppConroller.getStatus);
-  app.get('/stat', AppConroller.getStat);
-  app.post('/users', UsersController.postNew);
-  app.get('/connect', AuthController.getConnect);
-  app.get('/disconnect', AuthController.getDisconnect);
-  app.get('/users/me', UsersController.getMe);
-  app.post('/files', FilesController.postUpload);
-  app.get('/files', FilesController.getIndex);
-  app.get('/files/:id', FilesController.getShow);
-};
+const router = express.Router();
 
-export default loadRoutes;
+router.get('/status', AppConroller.getStatus);
+router.get('/stat', AppConroller.getStat);
+router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
+router.post('/files', FilesController.postUpload);
+router.get('/files', FilesController.getIndex);
+router.get('/files/:id', FilesController.getShow);
+
+export default router;
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import loadRoutes from './routes';
+import router from './routes';
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 
-loadRoutes(app);
+// Load routes
+app.use('/', router);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
